c2f5ba4013d9b68ad824530cde84a6a45e9694f4
rasult22 <warrior.006@inbox.ru>
2025-03-05 03:44:18 +0500

    Add mobile configuration and assets for game scenes and popups

---
+++ game/config-mobile.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+
+/* istanbul ignore file */
+var __COCOS_CONFIG__ = {
+  id: 'app',
+  width: 580,
+  height: 910,
+  renderMode: 0,
+  frameRate: 60,
+  debugMode: 0,
+  platform: 'mobile',
+  showFPS: false,
+  requestDefaultTimeout: 30000
+};
